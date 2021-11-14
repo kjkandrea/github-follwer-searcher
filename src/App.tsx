4b9c7d0d32a4e36bc4702e82e8321b2192c2f657
jongkeun-kim <kjkandrea@gmail.com>
2021-11-14 15:34:30 +0900

    Delete unnesses

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import SearchForm from './components/SearchForm';
-
 function App() {
   return (
     <div className="App">
-      <SearchForm />
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

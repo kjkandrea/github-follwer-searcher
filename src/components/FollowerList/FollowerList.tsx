681922d7520c21818a334c9406232390b3861bd3
jongkeun-kim <kjkandrea@gmail.com>
2021-11-17 17:39:00 +0900

    Fix subscribed duplicate problem

--- src/components/FollowerList/FollowerList.tsx
+++ src/components/FollowerList/FollowerList.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import NoFollowers from './NoFollowers';
 
 import SFollowerList from '../../styles/components/FollowerList';
-import { BehaviorSubject, catchError, of } from 'rxjs';
+import { BehaviorSubject, catchError, of, map } from 'rxjs';
 import { request } from '../../api/http';
 
 interface FollowerListProps {
@@ -14,13 +14,16 @@ const FollowerList: React.FC<FollowerListProps> = ({ usernameSubject }) => {
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
-    usernameSubject.subscribe(username => {
+    const subscription = usernameSubject.subscribe(username => {
       if (!username) return;
       request(`/users/${username}/followers`)
         .pipe(
           catchError(() => {
             console.log('error'); // TODO: 여기서 catch 가 안되네.. 에러핸들하는 방법을 내가 모르는듯
             return of([]);
+          }),
+          map(v => {
+            return v;
           }),
         )
         .subscribe({
@@ -29,6 +32,8 @@ const FollowerList: React.FC<FollowerListProps> = ({ usernameSubject }) => {
         });
       setUsername(username);
     });
+
+    return () => subscription.unsubscribe();
   }, []);
 
   return (
